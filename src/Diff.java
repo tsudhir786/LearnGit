e9b0f83ce805191b43fd6a68ac14a7e54be8d5ea
Tirumareddy, Sudhir <stirumareddy@netsuite.com>
2021-07-24 22:05:07 +0530

    Learn Git second commit

---
+++ src/Diff.java
@@ -0,0 +1,11 @@
+
+public class Diff {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 5;
+		int sumTotal = a - b;
+		System.out.println("Total value is " + sumTotal);
+	}
+
+}
