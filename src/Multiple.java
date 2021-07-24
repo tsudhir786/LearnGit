ca513c0bbd384648e83594439e28c8bb23541da3
Tirumareddy, Sudhir <stirumareddy@netsuite.com>
2021-07-24 22:20:16 +0530

    My First commit on sudhir/multiply branch , added multiple.java

---
+++ src/Multiple.java
@@ -0,0 +1,10 @@
+
+public class Multiple {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 5;
+		int sumTotal = a * b;
+		System.out.println("Total value is " + sumTotal);
+	}
+}
