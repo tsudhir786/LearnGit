ec82bd4015dcabe97cd463792d718d622bcfe074
Tirumareddy, Sudhir <stirumareddy@netsuite.com>
2021-07-24 22:06:57 +0530

    Learn Git Third commit ,Modified Sum.java

--- src/Sum.java
+++ src/Sum.java
@@ -5,7 +5,7 @@ public class Sum {
 		int a = 5;
 		int b = 5;
 		int sumTotal = a + b;
-		System.out.println("Total value is" + sumTotal);
+		System.out.println("Total value is " + sumTotal);
 	}
 
 }
