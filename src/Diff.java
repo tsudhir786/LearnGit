2093b8f7fb03a9123e4e633dff37ffa3cb732ffe
Tirumareddy, Sudhir <stirumareddy@netsuite.com>
2021-07-24 23:12:32 +0530

    Modified diffTotal variable

--- src/Diff.java
+++ src/Diff.java
@@ -4,8 +4,8 @@ public class Diff {
 	public static void main(String[] args) {
 		int a = 5;
 		int b = 5;
-		int sumTotal = a - b;
-		System.out.println("Total value is " + sumTotal);
+		int diffTotal = a - b;
+		System.out.println("Total value is " + diffTotal);
 	}
 
 }
