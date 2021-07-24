4c347fec6d7032e211910473bf5ad76c6b459526
Tirumareddy, Sudhir <stirumareddy@netsuite.com>
2021-07-24 23:48:09 +0530

    Hashmap.java file added to dev branch

---
+++ src/Hashmap.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class Hashmap {
+
+	public static void main(String[] args) {
+		HashMap<String, Integer> hashmap = new HashMap<>();
+		hashmap.put("Sudhir", 1);
+		hashmap.put("Shyam", 2);
+		hashmap.put("Siri", 3);
+		System.out.println(hashmap);
+
+	}
+
+}
